f06d6c01509135f501e8599ddf37552622e75f15
jordan <jordan@driedsponge.net>
2025-02-03 15:18:27 -0800

    work with colors

--- components/Navbar/Navbar.vue
+++ components/Navbar/Navbar.vue
@@ -3,15 +3,15 @@ import { ref } from "vue";
 
 let menuBarOpen = ref(false);
 const mobileLink =
-	"block py-1 border-2 rounded-md hover:bg-black hover:text-white transition-colors duration-200 ease-in-out w-3/5 text-center border-black";
+	"block py-1 border-2 text-base-content rounded-md hover:bg-base-content hover:text-base-100 transition-colors duration-200 ease-in-out w-3/5 text-center border-base-content";
 const desktopLink =
-	"px-2 py-0.5 border-2 rounded-md hover:bg-black hover:text-white transition-colors duration-200 ease-in-out border-black";
+	"px-2 py-0.5 border-2 text-base-content rounded-md hover:bg-base-content hover:text-base-100 transition-colors duration-200 ease-in-out border-base-content";
 const isDark = useDark();
 const toggleDark = useToggle(isDark);
 </script>
 <template>
 	<div class="sticky top-0 z-50 will-change-transform">
-		<nav class="hidden items-center justify-center border-b-2 bg-gray-200 py-3 md:flex">
+		<nav class="hidden items-center justify-center bg-base-300 py-3 md:flex">
 			<div class="container flex flex-row justify-between">
 				<div class="flex flex-row items-center space-x-5">
 					<img
@@ -19,7 +19,7 @@ const toggleDark = useToggle(isDark);
 						class="max-h-12"
 						alt="Husky Robotics Logo"
 					/>
-					<p class="text-lg font-extrabold uppercase">Husky Robotics</p>
+					<p class="text-lg font-extrabold uppercase text-base-content">Husky Robotics</p>
 				</div>
 				<div class="flex flex-row items-center space-x-6 lg:space-x-8">
 					<NuxtLink
@@ -57,7 +57,7 @@ const toggleDark = useToggle(isDark);
 				</div>
 			</div>
 		</nav>
-		<nav class="flex items-center justify-center border-b-2 bg-gray-200 px-4 py-3 md:hidden">
+		<nav class="flex items-center justify-center border-b-2 bg-base-300 px-4 py-3 md:hidden border-base-content">
 			<div class="container flex flex-row items-center justify-between">
 				<div class="flex flex-row items-center space-x-5">
 					<img
@@ -65,12 +65,12 @@ const toggleDark = useToggle(isDark);
 						class="max-h-12"
 						alt="Husky Robotics Logo"
 					/>
-					<p class="text-lg font-extrabold uppercase">Husky Robotics</p>
 				</div>
-				<button
+        <p class="text-lg font-extrabold uppercase text-base-content">Husky Robotics</p>
+        <button
 					role="button"
 					@click="menuBarOpen = !menuBarOpen"
-					class="w-10 rounded-md border-2 border-black py-0.5 text-lg font-extrabold transition-colors duration-100 ease-in-out active:bg-black active:text-white"
+					class="w-10 rounded-md border-2 text-base-content border-base-content py-0.5 text-lg font-extrabold transition-colors duration-100 ease-in-out active:bg-base-content active:text-base-300"
 				>
 					<i
 						class="fa-solid fa-bars"
@@ -84,7 +84,7 @@ const toggleDark = useToggle(isDark);
 			</div>
 		</nav>
 		<div
-			class="flex flex-col items-center space-y-5 border-b-2 bg-gray-200 py-5 transition-all duration-200 ease-in-out md:hidden"
+			class="flex flex-col items-center space-y-5 border-b-2 border-base-content bg-base-200 py-5 transition-all duration-200 ease-in-out md:hidden"
 			:class="{ 'opacity-0': !menuBarOpen }"
 		>
 			<NuxtLink
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+useSeoMeta({
+  title:"Home | Husky Robotics",
+  description: "Husky Robotics provides UW students the opportunity to develop skills as part of a large interdisciplinary science and engineering project. We turn our member's passion for robotics, space, science and engineering into real-world experience, offering a unique chance to have hands-on time in a variety of engineering and engineering-adjacent fields."
+})
+</script>
 <template>
-	<h1 class="mt-100">Husky Robotics</h1>
+  <div class="container w-full text-center mt-30">
+    <h1 class="font-extrabold text-8xl  from-primary to-secondary text-transparent bg-gradient-to-b bg-clip-text">
+      HUSKY ROBOTICS
+    </h1>
+  </div>
 </template>
