db5e606ae4cc5bf9f866e66dde6ebcbe0a8f6e31
jordan <jordan@driedsponge.net>
2025-02-07 15:20:20 -0800

    work on footer

--- app.vue
+++ app.vue
@@ -1,14 +1,18 @@
 <script lang="ts" setup>
+
 useSeoMeta({
 	title: "Husky Robotics"
 });
 </script>
 <template>
 	<Html lang="en" />
-	<Navbar />
-	<div class="flex flex-col items-center justify-center">
-		<main class="container">
-			<NuxtPage />
-		</main>
-	</div>
+  <div class="flex min-h-screen flex-col">
+    <Navbar />
+    <div class="flex flex-col items-center flex-1">
+      <main class="container">
+        <NuxtPage />
+      </main>
+    </div>
+    <Footer />
+  </div>
 </template>
---
+++ components/Footer.vue
@@ -0,0 +1,72 @@
+<template>
+	<footer class="mt-5 border-t-2 border-t-base-300 bg-base-100 py-8 md:px-2 lg:px-0">
+		<div
+			class="container mx-auto flex flex-col content-center items-center justify-between space-y-6 px-3 align-middle md:flex-row md:space-y-0 xl:px-0"
+		>
+			<div class="space-y-3 text-center text-base-content md:text-left">
+				<div>
+					<h1 class="font-bold">Contact Us:</h1>
+					<NuxtLink
+						href="mailto:uwrobots@uw.edu"
+						class="hover:underline"
+						>uwrobots@uw.edu</NuxtLink
+					>
+				</div>
+			</div>
+			<p class="space-x-4 text-base-content">
+				<NuxtLink
+					href="#"
+					class="coolDrop"
+				>
+					<i class="fa-brands fa-2x fa-instagram"></i>
+				</NuxtLink>
+				<NuxtLink
+					href="#"
+					class="coolDrop"
+				>
+					<i class="fa-brands fa-2x fa-facebook"></i>
+				</NuxtLink>
+
+				<NuxtLink
+					href="#"
+					class="coolDrop"
+				>
+					<i class="fa-brands fa-2x fa-linkedin"></i>
+				</NuxtLink>
+
+				<NuxtLink
+					href="#"
+					class="coolDrop"
+				>
+					<i class="fa-brands fa-2x fa-youtube"></i>
+				</NuxtLink>
+
+				<NuxtLink
+					href="#"
+					class="coolDrop"
+				>
+					<i class="fa-brands fa-2x fa-github"></i>
+				</NuxtLink>
+			</p>
+			<div class="space-y-2">
+				<p class="text-center text-3xl font-extrabold text-white md:text-right">
+					<img
+						src="~/assets/images/logo.png"
+						class="inline max-h-12"
+					/>
+				</p>
+				<p class="text-xs text-gray-400">© 2025 Husky Robotics</p>
+			</div>
+		</div>
+	</footer>
+</template>
+<style>
+@reference "~/assets/css/main.css";
+.coolDrop:hover {
+	@apply transition-all duration-300 ease-in-out hover:text-primary dark:hover:text-primary-400;
+	text-shadow:
+		2px 2px 0 var(--color-alt),
+		0 0 80px var(--color-primarysh);
+}
+</style>
+<script setup lang="ts"></script>
