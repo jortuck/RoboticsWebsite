050caa4765f983f17bfde3d8b557856e4beda346
jordan <jordan@driedsponge.net>
2025-02-13 14:34:07 -0800

    loop for links on footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,38 @@
+<script setup lang="ts">
+type SocialLink = {
+	name: string;
+	icon: string;
+	url: string;
+};
+
+const links: SocialLink[] = [
+	{
+		icon: "fa-instagram",
+		url: "https://www.instagram.com/uwrobots/",
+		name: "@uwrobots on Instagram."
+	},
+	{
+		icon: "fa-facebook",
+		url: "https://www.facebook.com/uwrobots/",
+		name: "@uwrobots on Facebook."
+	},
+	{
+		icon: "fa-linkedin",
+		url: "https://www.linkedin.com/company/husky-robotics/",
+		name: "@husky-robotics on LinkedIn."
+	},
+	{
+		icon: "fa-youtube",
+		url: "https://www.youtube.com/channel/UC274hSR7LTSRfsH4riugKBw",
+		name: "Husky Robotics on YouTube."
+	},
+	{
+		icon: "fa-github",
+		url: "https://github.com/huskyroboticsteam",
+		name: "@huskyroboticsteam on GitHub."
+	}
+];
+</script>
 <template>
 	<footer class="mt-5 border-t-2 border-t-base-300 bg-base-100 py-8 md:px-2 lg:px-0">
 		<div
@@ -15,42 +50,12 @@
 			</div>
 			<p class="flex-none space-x-4 text-base-content">
 				<NuxtLink
+					v-for="link in links"
 					target="_blank"
-					to="https://www.instagram.com/uwrobots/"
-					aria-label="@uwrobots on Instagram."
+					:to="link.url"
+					:aria-label="link.name"
 				>
-					<i class="fa-brands fa-2x fa-instagram"></i>
-				</NuxtLink>
-				<NuxtLink
-					target="_blank"
-					to="https://www.facebook.com/uwrobots/"
-					aria-label="@uwrobots on Facebook."
-				>
-					<i class="fa-brands fa-2x fa-facebook"></i>
-				</NuxtLink>
-
-				<NuxtLink
-					to="https://www.linkedin.com/company/husky-robotics/"
-					target="_blank"
-					aria-label="@husky-robotics on LinkedIn."
-				>
-					<i class="fa-brands fa-2x fa-linkedin"></i>
-				</NuxtLink>
-
-				<NuxtLink
-					target="_blank"
-					to="https://www.youtube.com/channel/UC274hSR7LTSRfsH4riugKBw"
-					aria-label="Husky Robotics on YouTube."
-				>
-					<i class="fa-brands fa-2x fa-youtube"></i>
-				</NuxtLink>
-
-				<NuxtLink
-					target="_blank"
-					to="https://github.com/huskyroboticsteam"
-					aria-label="@huskyroboticsteam on GitHub."
-				>
-					<i class="fa-brands fa-2x fa-github"></i>
+					<i :class="'fa-brands fa-2x ' + link.icon"></i>
 				</NuxtLink>
 			</p>
 			<div class="flex-1 space-y-2">
@@ -75,4 +80,3 @@ a:hover {
 	@apply text-primary transition-colors duration-300 ease-in-out dark:text-alt;
 }
 </style>
-<script setup lang="ts"></script>
