87d90219d375e9e0b66691ddc4554166730580c2
jordan <jordan@driedsponge.net>
2025-02-14 13:27:15 -0800

    add base for other pages

---
+++ pages/events/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="container mx-auto my-24 w-full space-y-20 text-center">
+		<h1
+			class="coolDropSmall font-logo text-4xl font-extrabold text-primary uppercase select-none md:text-6xl"
+		>
+			Events Will Go Here
+		</h1>
+	</div>
+</template>
---
+++ pages/team/index.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="container mx-auto my-24 w-full space-y-20 text-center">
+		<h1
+			class="coolDropSmall font-logo text-4xl font-extrabold text-primary uppercase select-none md:text-6xl"
+		>
+			Team Will Go Here
+		</h1>
+	</div>
+</template>
+<script setup lang="ts"></script>
