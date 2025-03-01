caf0999eab5f1a6600475c95401a6619b3be89d0
Jordan <jordan@driedsponge.net>
2025-03-01 00:04:42 -0800

    install nuxt content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
 	devtools: { enabled: true },
-	modules: ["@vueuse/nuxt", "@nuxt/image"],
+	modules: ["@vueuse/nuxt", "@nuxt/image", "@nuxt/content"],
 	vite: {
 		plugins: [tailwindcss()]
 	},
