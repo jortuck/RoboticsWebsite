332010861b96efddca7f729012bb961c42c5b3f4
Jordan <jordan@driedsponge.net>
2025-04-02 23:04:50 -0700

    Begin work on robots page

--- pages/robots/index.vue
+++ pages/robots/index.vue
@@ -1,10 +1,28 @@
 <template>
-	<div class="container mx-auto my-24 w-full space-y-20 text-center">
-		<h1
-			class="coolDropSmall font-logo text-4xl font-extrabold text-primary uppercase select-none md:text-6xl"
+	<div class="container mx-auto my-24 w-full space-y-20">
+		<header>
+			<h1
+				class="coolDropSmall text-center font-logo text-4xl font-extrabold text-primary uppercase select-none md:text-6xl"
+			>
+				Robots
+			</h1>
+		</header>
+		<section
+			v-for="i in 5"
+			class="mx-4 space-y-8 lg:mx-0"
 		>
-			Robots
-		</h1>
+			<SectionHeader> Oversight </SectionHeader>
+			<div>
+				<h3 class="text-xl text-base-content md:text-2xl lg:text-3xl">About</h3>
+				<p>3rd Place at CIRC 2024</p>
+			</div>
+			<div>
+				<h3 class="text-xl text-base-content md:text-2xl lg:text-3xl">Images</h3>
+			</div>
+			<div>
+				<h3 class="text-xl text-base-content md:text-2xl lg:text-3xl">Video</h3>
+			</div>
+		</section>
 	</div>
 </template>
 <script setup lang="ts">
