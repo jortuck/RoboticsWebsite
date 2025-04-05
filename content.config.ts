6aa0325643a12975063f5011e5ccc6f9f29124bf
Jordan <jordan@driedsponge.net>
2025-04-04 23:27:35 -0700

    Begin robots content

--- content.config.ts
+++ content.config.ts
@@ -11,6 +11,17 @@ export default defineContentConfig({
 				members: z.array(z.string()),
 				leads: z.array(z.object({ name: z.string(), position: z.string(), image: z.string() }))
 			})
+		}),
+		robots: defineCollection({
+			source: "robots/**.json",
+			type: "data",
+			schema: z.object({
+				name: z.string(),
+				years: z.string(),
+				description: z.string().optional(),
+				images: z.array(z.string()).optional(),
+				video: z.string().optional()
+			})
 		})
 	}
 });
