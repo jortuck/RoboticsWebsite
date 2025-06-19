c13a1e52927d35e61a33cf3df12bed70f99fcc9b
Jordan Tucker <jordan@jortuck.com>
2025-06-18 18:10:19 -0700

    remove unused content definitions

--- content.config.ts
+++ content.config.ts
@@ -2,16 +2,6 @@ import { defineCollection, defineContentConfig, z } from "@nuxt/content";
 
 export default defineContentConfig({
 	collections: {
-		subsystems: defineCollection({
-			source: "team/subsystems/**.yaml",
-			type: "data",
-			schema: z.object({
-				name: z.string(),
-				description: z.string(),
-				members: z.array(z.string()),
-				leads: z.array(z.object({ name: z.string(), position: z.string(), image: z.string() }))
-			})
-		}),
 		teams: defineCollection({
 			source: "team/**.yaml",
 			type: "data",
@@ -30,25 +20,6 @@ export default defineContentConfig({
 				description: z.string(),
 				link: z.string().url(),
 				img: z.string()
-			})
-		}),
-		robots: defineCollection({
-			source: "robots/**.yaml",
-			type: "data",
-			schema: z.object({
-				name: z.string(),
-				years: z.string(),
-				description: z.string().optional(),
-				images: z.array(z.string()).optional(),
-				video: z.string().optional() // YouTube Video ID
-			})
-		}),
-		posts: defineCollection({
-			type: "page",
-			source: "news-and-events/**.md",
-			schema: z.object({
-				date: z.string().date(),
-				image: z.string()
 			})
 		})
 	}
