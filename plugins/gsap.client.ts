1884380735e0ec689841b131eabf850b37c66b7b
Jordan Tucker <jordan@jortuck.com>
2025-06-18 19:20:47 -0700

    remove dumb import

--- plugins/gsap.client.ts
+++ plugins/gsap.client.ts
@@ -3,7 +3,6 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import is from "@sindresorhus/is";
 export default defineNuxtPlugin(() => {
 	gsap.registerPlugin(SplitText);
 	gsap.registerPlugin(ScrollTrigger);
