dc5a2fa434727e3cfe029188f5f34435695be138
Jordan Tucker <jordan@jortuck.com>
2025-07-16 13:29:45 -0700

    update join

--- app/pages/join.vue
+++ app/pages/join.vue
@@ -54,37 +54,39 @@ function closeFAQ(element: Element, done: () => void) {
 					);
 				"
 			>
-				<RoboticsContainer class="space-y-8 py-20 md:py-30">
-					<div class="space-y-8 md:max-w-1/2">
-						<h1 class="text-6xl leading-18 font-extrabold text-secondary lg:text-7xl">
+				<RoboticsContainer class="md:py-30 space-y-8 py-20">
+					<div class="md:max-w-1/2 space-y-8">
+						<h1 class="leading-18 text-secondary text-6xl font-extrabold lg:text-7xl">
 							Get Involved With<br />Husky Robotics
 						</h1>
-						<p class="pb-5 text-lg text-secondary md:max-w-1/2">
-							Husky Robotics' Prometheus rover seeks signs of life and collects samples of rock and
-							regolith for possible Earth return.
+						<p class="text-secondary md:max-w-1/2 pb-5 text-lg">
+							Join our team of 100+ talented UW engineers: gain industry experience, collaborate in
+							a very strong team culture, build the skills to launch into the world after college
 						</p>
 						<p class="invisible space-x-3 align-middle">
-							<span class="font-roboto font-medium tracking-robotics text-secondary"
+							<span class="font-roboto tracking-robotics text-secondary font-medium"
 								>University of Washington - Seattle</span
 							>
 						</p>
 					</div>
 				</RoboticsContainer>
-				<p class="container mx-auto px-4 py-8 text-xs text-zinc-400">
-					<NuxtLink
-						class="underline"
-						to="https://photojournal.jpl.nasa.gov/catalog/PIA26556"
-						target="_blank"
-						>PIA26556 – Mars Panorama from Perseverance</NuxtLink
-					>
-					(courtesy of NASA/JPL-Caltech)
-					<br />
-					<br />
-					These images are used for educational and academic purposes only, without any commercial
-					intent, and in full compliance with NASA’s Media Usage Guidelines. NASA does not endorse
-					this website or its contents. We gratefully acknowledge the work of the mission teams and
-					image processors whose contributions make these resources publicly available.
-				</p>
+				<RoboticsContainer>
+					<p class="container mx-auto px-4 py-8 text-xs text-zinc-400">
+						<NuxtLink
+							class="underline"
+							to="https://photojournal.jpl.nasa.gov/catalog/PIA26556"
+							target="_blank"
+							>PIA26556 – Mars Panorama from Perseverance</NuxtLink
+						>
+						(courtesy of NASA/JPL-Caltech)
+						<br />
+						<br />
+						These images are used for educational and academic purposes only, without any commercial
+						intent, and in full compliance with NASA’s Media Usage Guidelines. NASA does not endorse
+						this website or its contents. We gratefully acknowledge the work of the mission teams
+						and image processors whose contributions make these resources publicly available.
+					</p>
+				</RoboticsContainer>
 			</div>
 		</section>
 		<section class="bg-primary">
@@ -144,7 +146,7 @@ function closeFAQ(element: Element, done: () => void) {
 				<div class="flex flex-col space-y-8">
 					<div class="flex flex-col items-center gap-10 lg:flex-row">
 						<p
-							class="flex-1 border-b-1 border-b-neutral-400 py-8 text-xl leading-10 font-medium lg:text-2xl"
+							class="border-b-1 flex-1 border-b-neutral-400 py-8 text-xl font-medium leading-10 lg:text-2xl"
 						>
 							"Husky Robotics gave me hands-on experience I couldn't get from classes. Every day
 							here is about trying new ideas and solving interesting problems."
@@ -159,7 +161,7 @@ function closeFAQ(element: Element, done: () => void) {
 					</div>
 					<div class="flex flex-col items-center gap-10 lg:flex-row">
 						<p
-							class="flex-1 border-b-1 border-b-neutral-400 py-8 text-xl leading-10 font-medium lg:text-2xl"
+							class="border-b-1 flex-1 border-b-neutral-400 py-8 text-xl font-medium leading-10 lg:text-2xl"
 						>
 							"After going to admitted students day last year and seeing the rover and the space
 							focus I knew I wanted to join Husky Robotics."
@@ -174,7 +176,7 @@ function closeFAQ(element: Element, done: () => void) {
 					</div>
 					<div class="flex flex-col items-center gap-10 lg:flex-row">
 						<p
-							class="flex-1 border-b-1 border-b-neutral-400 py-8 text-xl leading-10 font-medium lg:text-2xl"
+							class="border-b-1 flex-1 border-b-neutral-400 py-8 text-xl font-medium leading-10 lg:text-2xl"
 						>
 							"Our partners really invest in our futures. Being part of this team opened up
 							opportunities I never expected."
@@ -195,7 +197,7 @@ function closeFAQ(element: Element, done: () => void) {
 			class="bg-black"
 		>
 			<RoboticsContainer class="space-y-8 py-20 xl:w-1/2">
-				<h2 class="text-5xl font-bold text-secondary">Application Cycle: 2025-2026</h2>
+				<h2 class="text-secondary text-5xl font-bold">Application Cycle: 2025-2026</h2>
 				<p class="text-lg text-zinc-100">
 					Our main admissions take place during Autumn and Winter quarter, when we welcome new
 					members to join our mission. Decisions are released by the third week of each cycle, and
@@ -214,7 +216,7 @@ function closeFAQ(element: Element, done: () => void) {
 				<p class="font-roboto tracking-robotics">Husky Robotics</p>
 				<h2 class="text-6xl font-bold">FAQs</h2>
 				<div
-					class="my-8 flex flex-col divide-y-1 divide-neutral-400 border-y-1 border-y-neutral-400"
+					class="divide-y-1 border-y-1 my-8 flex flex-col divide-neutral-400 border-y-neutral-400"
 				>
 					<div
 						v-for="faq in faqs"
@@ -222,7 +224,7 @@ function closeFAQ(element: Element, done: () => void) {
 						:key="faq.title"
 					>
 						<h3
-							class="text-xl font-semibold select-none hover:cursor-pointer md:text-2xl"
+							class="select-none text-xl font-semibold hover:cursor-pointer md:text-2xl"
 							role="tab"
 							tabindex="0"
 							@keydown.enter="setExpanded(faq.title)"
@@ -262,7 +264,7 @@ function closeFAQ(element: Element, done: () => void) {
 <style scoped>
 @reference "~/assets/css/main.css";
 .linklist > button {
-	@apply cursor-pointer border-b-1 border-b-neutral-300 py-2 text-left text-lg text-neutral-300;
+	@apply border-b-1 cursor-pointer border-b-neutral-300 py-2 text-left text-lg text-neutral-300;
 	@apply transition-all duration-300 ease-in-out;
 	@apply hover:border-b-secondary hover:text-secondary;
 }
